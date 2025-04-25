512d4e93cf13dea3ebdfeb52c46e4310242185aa
nosa <wadidessy@gmail.com>
2025-04-25 01:02:02 +0000

    deleted env.example, docker-entrypoint.sh, prometheus.yml

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set correct permissions after volume mount
-#chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-#chmod -R ug+rwx /var/www/storage /var/www/bootstrap/cache
-
-# Run the original CMD (php-fpm)
-#exec "$@"
